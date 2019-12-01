6e403d727688ea5a310b98ca19d0b24422e562ea
陈光文 <1048496188@qq.com>
2019-12-02 00:51:37 +0800

    [fature]更新私信聊天子系统

--- client/src/app/components/chat/chat.component.ts
+++ client/src/app/components/chat/chat.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient, HttpHeaders } from "@angular/common/http"
+import { ReqProto } from 'src/app/services/proto'
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
 export class ChatComponent implements OnInit {
-
-  constructor() { }
+  public httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+  public senderSelected: number;
+  public senderList: any;
+  public msgInput: string;
+  public msgList: any;
+  constructor(
+    public http: HttpClient,
+    private reqProto: ReqProto,
+  ) { }
 
   ngOnInit() {
-  }
+    // 获取所有发送私信的人
 
+    // 获取与某人的所有聊天记录
+  }
+  getSanderList() {
+    this.reqProto.action = "POST";
+    this.reqProto.data = {
+      uid: 1,
+    }
+    this.http.post("/api/getSenderList", this.reqProto, this.httpOptions).subscribe((res: any) => {
+      this.senderList = res.data;
+      console.log(res);
+    })
+  }
+  // 回复私信
+  send() {
+    this.http.post("/api/sendMsg", this.reqProto, this.httpOptions).subscribe((res: any) => {
+      this.msgList.push(res.data);
+      console.log(res);
+    })
+  }
 }
